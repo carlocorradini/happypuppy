7cd8e25f5ed6607f53bab26100751c083d7facc0
Carlo Corradini <c.corradini@ad.enginsoft.com>
2020-03-28 20:59:43 +0100

    Fist repository commit with sign in layout example

---
+++ app/src/main/java/com/unitn/disi/lpsmt/happypuppy/MainActivity.java
@@ -0,0 +1,15 @@
+package com.unitn.disi.lpsmt.happypuppy;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_login);
+    }
+
+}
