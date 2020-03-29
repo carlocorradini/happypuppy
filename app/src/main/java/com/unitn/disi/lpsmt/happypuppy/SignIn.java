de53a092cc7ee4fe4ece21561f21c630dd970412
Carlo Corradini <c.corradini@ad.enginsoft.com>
2020-03-29 21:21:16 +0200

    Added Sign Up layout

---
+++ app/src/main/java/com/unitn/disi/lpsmt/happypuppy/SignIn.java
@@ -0,0 +1,40 @@
+package com.unitn.disi.lpsmt.happypuppy;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignIn extends AppCompatActivity {
+
+    private EditText inputUsername;
+    private EditText inputPassword;
+    private Button buttonSignIn;
+    private Button buttonSignUp;
+    private Button buttonForgotPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_sign_in);
+
+        inputUsername = findViewById(R.id.sign_in_input_username);
+        inputPassword = findViewById(R.id.sign_in_input_password);
+        buttonSignIn = findViewById(R.id.sign_in_button_sign_in);
+        buttonSignUp = findViewById(R.id.sign_in_button_sign_up);
+        buttonForgotPassword = findViewById(R.id.sign_in_button_forgot_password);
+
+        final Intent intent = new Intent(this, SignUp.class);
+        buttonSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
+    }
+
+}
--- app/src/main/java/com/unitn/disi/lpsmt/happypuppy/SignUp.java
+++ app/src/main/java/com/unitn/disi/lpsmt/happypuppy/SignUp.java
@@ -4,12 +4,12 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class SignUp extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_sign_in);
+        setContentView(R.layout.fragment_sign_up);
     }
 
 }
