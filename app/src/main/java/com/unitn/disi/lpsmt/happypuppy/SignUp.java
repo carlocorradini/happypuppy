c6b3146c598b2328c7a1e8f78e6013edbeea729e
Anthony Farina <anthony.farina@studenti.unitn.it>
2020-03-30 17:44:21 +0200

    Fix link signin signup

--- app/src/main/java/com/unitn/disi/lpsmt/happypuppy/SignUp.java
+++ app/src/main/java/com/unitn/disi/lpsmt/happypuppy/SignUp.java
@@ -1,15 +1,44 @@
 package com.unitn.disi.lpsmt.happypuppy;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SignUp extends AppCompatActivity {
 
+    private EditText inputFullName;
+    private EditText inputUser;
+    private EditText inputEMail;
+    private EditText inputPassword;
+    private EditText inputRepeatPassword;
+    private Button buttonSignIn;
+    private Button buttonSignUp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_sign_up);
-    }
 
+        inputFullName = findViewById(R.id.sign_up_input_full_name);
+        inputUser = findViewById(R.id.sign_up_input_username);
+        inputEMail = findViewById(R.id.sign_up_input_email);
+        inputPassword = findViewById(R.id.sign_up_input_password);
+        inputRepeatPassword = findViewById(R.id.sign_up_input_password_repeat);
+
+        buttonSignIn = findViewById(R.id.sign_up_button_sign_in);
+        buttonSignUp = findViewById(R.id.sign_up_button_sign_up);
+
+        buttonSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), SignIn.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
