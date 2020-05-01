61a06ee6d46d59fa1ad65f7203ee435b81fc1b86
Carlo Corradini <c.corradini@ad.enginsoft.com>
2020-05-01 15:05:02 +0200

    Added final class to adapters

--- app/src/main/java/com/unitn/disi/lpsmt/happypuppy/api/adapter/serializer/JWTSerializer.java
+++ app/src/main/java/com/unitn/disi/lpsmt/happypuppy/api/adapter/serializer/JWTSerializer.java
@@ -8,7 +8,7 @@ import com.google.gson.JsonSerializer;
 
 import java.lang.reflect.Type;
 
-public class JWTSerializer implements JsonSerializer<JWT> {
+public final class JWTSerializer implements JsonSerializer<JWT> {
     @Override
     public JsonElement serialize(JWT src, Type typeOfSrc, JsonSerializationContext context) {
         return new JsonPrimitive(src.toString());
